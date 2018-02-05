10cd7d891002fad88bcfbcd28be9165740278aae
ChenHan Leonard Yang <yang313@atrihub.io>
2018-02-05 12:25:58 -0800

    feat(Angular): Making an interface for registering new users
    Making an interface for registering new users
    Controlling the interface with RegisterController
    Registration Routes and Modules
    
    ICTRAINING-150

---
+++ static/javascripts/authentication/controllers/register.controller.js
@@ -0,0 +1,18 @@
+(function (){
+    'user strict';
+
+    angular
+        .module('thinkster.aiuthentication.controllers')
+        .controller('RegisterController, RegisterController');
+
+    RegisterController.$inject = ['$location', '$scope', 'Authentication'];
+
+    function RegisterController($location, $scope, Authentication){
+        var vm = this;
+        vm.register = register;
+
+        function register() {
+            Authentication.register(vm.email, vm.password, vm.username);
+        }
+    }
+})();
